637c546d0a373a1cb1343e5cd848716492a08512
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-21 19:20:50 -0300

    :lipstick: feat: created  GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "./index.css";
+import { GlobalStyle } from "./styles/global.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
