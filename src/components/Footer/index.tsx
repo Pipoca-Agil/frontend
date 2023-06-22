1631d0b82f6a2f052d2e68bc8399e0aec2b507b2
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-22 17:35:46 -0300

    :sparkles: feat: New component - Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./style";
+
+export function Footer() {
+  return (
+    <S.Container>
+      <S.NavFooter></S.NavFooter>
+    </S.Container>
+  );
+}
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,18 @@
+import { w } from "windstitch";
+
+export const Container = w.footer(`
+  w-full
+  flex
+  items-center 
+  justify-center 
+  bg-custom-BLACK-100
+`);
+
+export const NavFooter = w.div(`
+  px-4 
+  lg:px-[11.375rem] 
+  flex  container 
+  items-center 
+  h-[4.375rem]
+  justify-between
+`);
