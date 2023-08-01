17c519203feeae50ed272c0155dd8587313dbeaa
unknown <brennerpaiva@gmail.com>
2023-08-01 15:00:26 -0300

    refactor: added header inside BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomePage from './pages/Home';
 import NotFound from './pages/NotFound';
 import Register from './pages/Register';
-
+import Header from './components/Header/Header';
+import {GlobalStyle} from './styles/Global';
 import './App.css';
 import Login from './pages/Login';
 import RegisterProvider from './context/registerProvider';
@@ -11,20 +12,23 @@ import ModalProvider from './context/ModalProvider';
 
 function App() {
 	return (
-		<RegisterProvider>
-			<ModalProvider>
-				<BrowserRouter>
-					<Routes>
-						<Route path='/' element={<HomePage />} />
-						<Route path='/home' element={<HomePage />} />
-						<Route path='*' element={<NotFound/>}/>
-						<Route path='/register' element= {<Register/>}/>
-						<Route path='/login' element= {<Login/>}/>
-					</Routes>
-				</BrowserRouter>
-
-			</ModalProvider>
-		</RegisterProvider>
+		<>
+			<GlobalStyle/>
+			<RegisterProvider>
+				<ModalProvider>
+					<BrowserRouter>
+						<Header />
+						<Routes>
+							<Route path='/' element={<HomePage />} />
+							<Route path='/home' element={<HomePage />} />
+							<Route path='*' element={<NotFound/>}/>
+							<Route path='/register' element= {<Register/>}/>
+							<Route path='/login' element= {<Login/>}/>
+						</Routes>
+					</BrowserRouter>
+				</ModalProvider>
+			</RegisterProvider>
+		</>
 	);
 }
 
