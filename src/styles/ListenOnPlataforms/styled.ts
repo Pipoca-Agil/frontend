0e8492bada0d405b3382bd76af59cf07ebd37a18
unknown <brennerpaiva@gmail.com>
2023-08-22 12:58:52 -0300

    create: add ListenOnPlatforms component

---
+++ src/styles/ListenOnPlataforms/styled.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import styled from 'styled-components';
+
+export const ListenOnPlatforms = styled.div`
+    padding-left: 2rem;
+`;
+
+export const Title = styled.h3`
+    margin-top: 3.9rem;
+    color: #B33B3B;
+    font-family: Maven Pro;
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 2.2rem; 
+`;
+
+export const Platform = styled.a`
+    text-decoration: none;
+    cursor: pointer;
+    display: flex;
+    margin-top: 1.5rem;
+`;
+
+export const PlatformIcon = styled.img`
+    width: 3.1rem;
+    height: 3.1rem;
+    margin-right: 1rem;
+    object-fit: contain;
+`;
+
+export const PlatformName = styled.p`
+    margin: auto 0;
+    line-height: 1.6875rem;
+    color: #525252;
+    font-family: Inter;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 600;
+    justify-content: center;
+`;
