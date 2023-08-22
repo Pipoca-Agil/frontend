7ed7bef2a995bfbfd38f885b3424852367e67b9c
unknown <brennerpaiva@gmail.com>
2023-08-22 13:04:44 -0300

    feat: add AudioMessage and ListenOnPlatforms in the PrincipalHeadline

--- src/components/PrincipalHeadline/index.tsx
+++ src/components/PrincipalHeadline/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-
 import * as S from '../../styles/PrincipalHeadline/styled';
+import AudioMessage from '../AudioMessage/index';
+import ListenOnPlatforms from '../ListenOnPlatforms/index';
 
 export default function PrincipalHeadline() {
 	return (
 		<S.Wrapper>
 			<S.Title>
-				PIPOCA ÁGIL
+				Pipoca Ágil
 			</S.Title>
 			<S.SubTitle>
-				O maior podcast do <br/>
-				mundo ágil!
+				O maior podcast do mundo ágil!
 			</S.SubTitle>
-			<S.ButtonDiv>
-				<a href='https://open.spotify.com/show/5J1scP1l7m7kXK6v5RZS7J?si=3ebb7f18b6aa4bba&nd=1' target='_blank' rel='noreferrer'>
-					<S.Button buttonType='type2'>Ouvir podcast</S.Button>
-				</a>
-				<a href='https://www.youtube.com/@PipocaAgil' target='_blank' rel='noreferrer'>
-					<S.Button buttonType='type1'>Conhecer o canal</S.Button>
-				</a>
-			</S.ButtonDiv>
+			<S.PositionWrapper>
+				<AudioMessage/>
+				<ListenOnPlatforms/>
+			</S.PositionWrapper>
 		</S.Wrapper>
 	);
 }
--- src/styles/PrincipalHeadline/styled.ts
+++ src/styles/PrincipalHeadline/styled.ts
@@ -1,71 +1,35 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    text-align: center;
     padding-block: 6rem;
     font-family: 'Inter';
     color: #2D2D2D;
 `;
 
 export const Title = styled.h1`
-    text-transform: uppercase;
-    font-family: 'Cherry Bomb One', cursive;
-    font-size: 8rem;
-    font-weight: 400;
-    font-style: normal;
     color: #B33B3B;
-    line-height: normal;
-    margin-bottom: 1.2rem;
+    text-align: center;
+    font-family: Maven Pro;
+    font-size: 4.5rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 4.95rem;
+    margin-bottom: 2.5rem;
 `;
 
 export const SubTitle = styled.h2`
-    font-size: 4.6rem;
-    font-style: normal;
-    font-weight: 600;
-    color: #2D2D2D;
-    line-height: normal;
-    margin-bottom: 4.6rem;
-`;
-
-export const ButtonDiv = styled.div`
-    display: flex;
-    gap: 2.4rem;
-    flex-direction: column;
-    width: 29.2rem;
-    margin: 0 auto;
-`;
-
-type ButtonProps = {
-	buttonType: string;
-};
-
-export const Button = styled.button<ButtonProps>`
-    ${props =>
-		props.buttonType === 'type1'
-    && css`
-      background-color: transparent;
-      color: #B33B3B;
-      border: 2px solid #B33B3B;
-    `};
-    ${props =>
-		props.buttonType === 'type2'
-    && css`
-      background-color: #B33B3B;
-      color: #FBFBFB;
-      border: 2px solid #B33B3B;
-    `};
-    width: 100%;
-    border-radius: 2.4rem;
-    padding: 1rem 3.2rem;
-    text-decoration: none;
+    color: #292929;
     text-align: center;
-    align-items: center;
-    justify-content: center;
-    font-size: 2.2rem;
-    font-weight: 500;
-    font-family: 'Inter';
-    cursor: pointer;
+    font-family: Maven Pro;
+    font-size: 3.5rem;
+    font-style: normal;
+    font-weight: 700;
+    margin-bottom: 2.8rem;
+`;
+
+export const PositionWrapper = styled.div`
+    margin: 0 auto;
 `;
