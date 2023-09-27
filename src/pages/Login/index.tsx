fb526bf419a4845159bd7e886934614e99ce5a74
daniel-s-souza <danielsantsouza94@gmail.com>
2023-09-27 13:51:41 -0300

    feat: implementa validação de email através de regex

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -33,6 +33,11 @@ export default function Login() {
     color: "#B33B3B",
   }
 
+  const emailValidation = (email: string) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+
   return (
     <Body>
     <LogoImage>
