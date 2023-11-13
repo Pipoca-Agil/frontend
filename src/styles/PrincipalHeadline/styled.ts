53c0b7d2008d016d516e630fb459ef6c28beb599
daniel-s-souza <danielsantsouza94@gmail.com>
2023-11-13 13:54:55 -0300

    feat: ajusta fonte do Subtitulo

--- src/styles/PrincipalHeadline/styled.ts
+++ src/styles/PrincipalHeadline/styled.ts
@@ -28,6 +28,10 @@ export const SubTitle = styled.h2`
     font-style: normal;
     font-weight: 700;
     margin-bottom: 2.8rem;
+
+    @media (max-width: 926px) and (max-height: 926px) {
+       font-size: 12px;
+      }
 `;
 
 export const PositionWrapper = styled.div`
