e90b8853e3a8e7b294db8f7094876317d2b4a4dc
daniel-s-souza <danielsantsouza94@gmail.com>
2023-11-13 16:21:00 -0300

    feat: implementa header responsivo

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -9,30 +9,35 @@ export const Wrapper = styled.div`
 `;
 
 export const Button = styled(Link)`
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
     text-align: center;
     text-decoration: none;
-    width: 17.875rem;
-    height: 2.6rem;
-    border-radius: 1.25rem;
+    width: 11.6rem;
+    height: 3.9rem;
+    border-radius: 2rem;
     background-color: #B33B3B;
     color: #F3F7F8;
     font-family: Inter;
     font-size: 1.3rem;
     font-weight: 700;
     line-height: 1.7rem; 
+    margin-left: 18rem;
+    box-shadow: 0px 2px 22px 0px rgba(0, 0, 0, 0.25);
 `;
 
 export const ImageLogo = styled.img`
   width: 54px;
   height: 46px;
+  margin-left: 1rem;
 `;
 
 export const ImageButton = styled.button`
   background-color: #FAEDED
   position: absolute;
+  margin-left: 1rem;
 `; 
 
 export const OptionsWapper = styled.div`
