1da28029b8595465beaaa005bc38661b0f9599e6
daniel-s-souza <danielsantsouza94@gmail.com>
2023-11-13 13:50:38 -0300

    feat: inicia responsividade mobile

--- src/styles/BalloonsAside/styled.ts
+++ src/styles/BalloonsAside/styled.ts
@@ -5,12 +5,23 @@ export const BalloonsWrapper = styled.div`
     display: flex;
     position: relative;
     width: 50rem;
+    
+    @media (max-width: 926px) and (max-height: 926px) {
+        margin: auto;
+        width: 45rem;
+      }
 `;
 
 export const LeftBalloon = styled.img`
     position: absolute;
     width: 15rem;
     
+    @media (max-width: 926px) and (max-height: 926px) {
+        position: relative;
+        left: 50%;
+        margin-left: 10%;
+        bottom: 105px;
+      }
 `;
 
 export const CenterBalloon = styled.img`
@@ -20,6 +31,11 @@ export const CenterBalloon = styled.img`
     transform: translateY(50%);
     left: 30%;
     bottom: 30%;
+
+    @media (max-width: 926px) and (max-height: 926px) {
+        position: absolute;
+        left: 10%;
+      }
 `;
 
 export const RightBalloon = styled.img`
@@ -28,4 +44,8 @@ export const RightBalloon = styled.img`
     transform: translateY(50%);
     left: 70%;
     bottom: 70%;
+
+    @media (max-width: 926px) and (max-height: 926px) {
+        display: none;
+      }
 `;
